ae424dd583be2b4eb020c1a3eeac1b5a9e15c8e8
admin <admin@DESKTOP-OQL8701>
2017-04-20 07:29:21 +0700

    Việt đẹp trai :">

---
+++ DrinkStore/Entities/Customer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DrinkStore.Entities
+{
+    class Customer
+    {
+        private int _customerID;
+
+        public int CustomerID
+        {
+            get { return _customerID; }
+            set { _customerID = value; }
+        }
+
+        private string _customerName;
+
+        public string CustomerName
+        {
+            get { return _customerName; }
+            set { _customerName = value; }
+        }
+
+        private int _phoneNumber;
+
+        public int PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public Customer()
+        {
+        }
+
+        public Customer(DataRow row)
+        {
+            this._customerID = (int)row["CustomerID"];
+            this._customerName = (string)row["CustomerName"];
+            this._phoneNumber = (int)row["PhoneNumber"];
+            this._email = (string)row["Email"];
+        }
+    }
+}
---
+++ DrinkStore/Entities/Import.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DrinkStore.Entities
+{
+    class Import
+    {
+        private int _importID;
+
+        public int ImportID
+        {
+            get { return _importID; }
+            set { _importID = value; }
+        }
+
+        private int _productID;
+
+        public int ProductID
+        {
+            get { return _productID; }
+            set { _productID = value; }
+        }
+
+        private int _qnt_Cask;
+
+        public int Qnt_Cask
+        {
+            get { return _qnt_Cask; }
+            set { _qnt_Cask = value; }
+        }
+
+        private int _qnt_Bottle;
+
+        public int Qnt_Bottle
+        {
+            get { return _qnt_Bottle; }
+            set { _qnt_Bottle = value; }
+        }
+
+        private DateTime _importDate;
+
+        public DateTime ImportDate
+        {
+            get { return _importDate; }
+            set { _importDate = value; }
+        }
+
+        private float _price_Base;
+
+        public float Price_Base
+        {
+            get { return _price_Base; }
+            set { _price_Base = value; }
+        }
+
+        public Import(int ImportID, int ProductID, DateTime ImportDate ,int Qnt_Cask, int Qnt_Bottle, int Price_Base)
+        {
+            this._importID = ImportID;
+            this._productID = ProductID;
+            this._importDate = ImportDate;
+            this._qnt_Cask = Qnt_Cask;
+            this._qnt_Bottle = Qnt_Bottle;
+            this._price_Base = Price_Base;
+        }
+
+        public Import(DataRow row)
+        {
+            this._importID = (int)row["ImportID"];
+            this._productID = (int)row["ProductID"];
+            this._importDate = (DateTime)row["ImportDate"];
+            this._qnt_Cask = (int)row["Qnt_Cask"];
+            this._qnt_Bottle = (int)row["Qnt_Bottle"];
+            this._price_Base = (int)row["Price_Base"];
+        }
+    }
+}
