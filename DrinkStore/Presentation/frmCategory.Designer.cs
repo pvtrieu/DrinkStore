02ec8ca3d6655286569bceb14495dc0c591e22ca
trieu <trieu@DESKTOP-1GEP48J>
2017-05-09 17:26:36 +0700

    aaa

--- DrinkStore/Presentation/frmCategory.Designer.cs
+++ DrinkStore/Presentation/frmCategory.Designer.cs
@@ -28,31 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.lblLogo = new System.Windows.Forms.Label();
             this.closeButton1 = new DrinkStore.Presentation.CloseButton();
+            this.txtUnitName = new System.Windows.Forms.TextBox();
+            this.lblUnitName = new System.Windows.Forms.Label();
+            this.pnlLogo.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(135, 80);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(163, 20);
+            this.txtName.TabIndex = 55;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(42)))), ((int)(((byte)(53)))));
+            this.lblName.Location = new System.Drawing.Point(31, 81);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(54, 17);
+            this.lblName.TabIndex = 54;
+            this.lblName.Text = "Name:";
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(45)))));
+            this.pnlLogo.Controls.Add(this.closeButton1);
+            this.pnlLogo.Controls.Add(this.lblLogo);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(331, 50);
+            this.pnlLogo.TabIndex = 56;
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.Font = new System.Drawing.Font("Lucida Fax", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(255)))));
+            this.lblLogo.Location = new System.Drawing.Point(12, 9);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(116, 25);
+            this.lblLogo.TabIndex = 1;
+            this.lblLogo.Text = "Category";
             // 
             // closeButton1
             // 
-            this.closeButton1.Location = new System.Drawing.Point(303, 12);
+            this.closeButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton1.Location = new System.Drawing.Point(295, 9);
             this.closeButton1.Name = "closeButton1";
             this.closeButton1.Size = new System.Drawing.Size(24, 24);
-            this.closeButton1.TabIndex = 0;
+            this.closeButton1.TabIndex = 57;
+            // 
+            // txtUnitName
+            // 
+            this.txtUnitName.Location = new System.Drawing.Point(135, 113);
+            this.txtUnitName.Name = "txtUnitName";
+            this.txtUnitName.Size = new System.Drawing.Size(163, 20);
+            this.txtUnitName.TabIndex = 58;
+            // 
+            // lblUnitName
+            // 
+            this.lblUnitName.AutoSize = true;
+            this.lblUnitName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnitName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(42)))), ((int)(((byte)(53)))));
+            this.lblUnitName.Location = new System.Drawing.Point(31, 116);
+            this.lblUnitName.Name = "lblUnitName";
+            this.lblUnitName.Size = new System.Drawing.Size(86, 17);
+            this.lblUnitName.TabIndex = 57;
+            this.lblUnitName.Text = "Unit name:";
             // 
             // frmCategory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 226);
-            this.Controls.Add(this.closeButton1);
+            this.ClientSize = new System.Drawing.Size(331, 172);
+            this.Controls.Add(this.txtUnitName);
+            this.Controls.Add(this.lblUnitName);
+            this.Controls.Add(this.pnlLogo);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmCategory";
             this.Text = "Category";
+            this.pnlLogo.ResumeLayout(false);
+            this.pnlLogo.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Panel pnlLogo;
         private CloseButton closeButton1;
+        private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.TextBox txtUnitName;
+        private System.Windows.Forms.Label lblUnitName;
     }
 }
